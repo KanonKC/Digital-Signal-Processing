cbfae3391312043627eff049688558cc3e5c9144
KanonKC <kanonkce@gmail.com>
2021-12-29 10:19:59 +0700

    Add Example

---
+++ 01-Introduction to MATLAB/example.m
@@ -0,0 +1,9 @@
+f = 200;
+tt = [0:1/(20*f):1];
+z = exp(i*2*pi*f*tt); 
+subplot(211) 
+plot(real(z))
+title('Real part of exp(j*2*pi*200*tt)')
+subplot(212)
+plot(imag(z))
+title('Imaginary part of exp(j*2*pi*200*tt)')
